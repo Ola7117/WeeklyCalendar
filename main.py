ff0487a94243cbca0a9e3765a941ac7d53998847
Aleksandra Hinc <olka.hinc@gmail.com>
2024-01-09 05:36:56 +0100

    Added current week's dates

--- main.py
+++ main.py
@@ -1,10 +1,120 @@
-from calendar import day_name
+from calendar import day_name, month_name
 from datetime import date
-from dateutil.relativedelta import relativedelta, MO
+from dateutil.relativedelta import MO, TU, WE, TH, FR, SA, SU, relativedelta
 from icalendar import Calendar
 from os.path import isfile
 from tkinter import *
 from tkinter import filedialog
+
+
+def display_calendar(file_path):
+    file = open(file_path, 'rb')
+    calendar = Calendar.from_ical(file.read())
+    events = []
+
+    for i in range(7):
+        events.append([])
+
+    for component in calendar.walk():
+        if component.name == 'VEVENT':
+            event_info, date_start = get_event_info(component)
+
+            for i in range(7):
+                find_events_this_week(date_start, events, event_info, i)
+
+    for i in range(7):
+        events[i].sort()
+        textboxes[i].config(state=NORMAL)
+
+    for i in range(7):
+        for j in range(len(events[i])):
+            textboxes[i].insert(END, events[i][j][0] + '-' + events[i][j][1] + '\n' + events[i][j][2] + '\n')
+        textboxes[i].config(state=DISABLED)
+
+    file.close()
+
+
+def find_events_this_week(date_start, events, event_info, weekday_nr):
+    weekday_abbr = [MO, TU, WE, TH, FR, SA, SU]
+    today = date.today()
+    if today.weekday() > weekday_nr and date_start == str(today + relativedelta(weekday=weekday_abbr[weekday_nr](-1))):
+        events[weekday_nr].append(event_info)
+    elif today.weekday() <= weekday_nr and date_start == str(today + relativedelta(weekday=weekday_nr)):
+        events[weekday_nr].append(event_info)
+
+
+def get_event_info(component):
+    time_start = str(component.get('dtstart'))[21:29]
+    time_end = str(component.get('dtend'))[21:29]
+    event_name = str(component.get('summary'))
+    event_info = [time_start, time_end, event_name]
+
+    date_start = str(component.get('dtstart'))[10:20]
+    date_end = str(component.get('dtstart'))[10:20]
+
+    if date_start != date_end:
+        event_info.append(date_end)
+
+    return event_info, date_start
+
+
+def initialize_window():
+    window = Tk()
+    window.title('Weekly Calendar')
+    window.resizable(False, False)
+
+    weekdays = ()
+    days = ()
+    text = ()
+    left = 5
+    right = 5
+
+    day_numbers = update_days_numbers()
+
+    for i in range(7):
+        weekdays = weekdays + (Label(text=day_name[i], font=('', 12)),)
+        days = days + (Label(text=day_numbers[i], font=('', 12)),)
+        text = text + (Text(height=10, width=30),)
+
+        if i < 4:
+            if i == 0:
+                left = 10
+            elif i == 3:
+                right = 10
+
+            weekdays[i].grid(row=0, column=i * 2, sticky=W, padx=(left, 5), pady=(10, 5))
+            days[i].grid(row=0, column=i * 2 + 1, sticky=E, padx=(5, right), pady=(10, 5))
+            text[i].grid(row=1, column=i * 2, columnspan=2, padx=(left, right), pady=(5, 5))
+
+        else:
+            if i == 4:
+                left = 10
+            elif i == 6:
+                right = 10
+
+            weekdays[i].grid(row=2, column=(i - 4) * 2, sticky=W, padx=(left, 5), pady=(5, 5))
+            days[i].grid(row=2, column=(i - 4) * 2 + 1, sticky=E, padx=(5, right), pady=(5, 5))
+            text[i].grid(row=3, column=(i - 4) * 2, rowspan=3, columnspan=2, padx=(left, right), pady=(5, 10))
+
+        text[i].config(state=DISABLED)
+
+    label_title = Label(text='Weekly Calendar', font=('', 16))
+    label_title.grid(row=3, column=6, columnspan=2, padx=10, pady=10)
+
+    month = month_name[date.today().month]
+    year = date.today().year
+
+    label_month = Label(text=str(month) + ' ' + str(year), font=('', 12))
+    label_month.grid(row=4, column=6, columnspan=2, padx=10, pady=10)
+
+    button = Button(
+        window,
+        text='Open an ICS file',
+        command=open_file
+    )
+    button.grid(row=5, column=6, columnspan=2, padx=10, pady=10)
+
+    return window, text
 
 
 def load():
@@ -18,28 +128,6 @@ def load():
         saved_file_path.close()
 
 
-def save(file_path):
-    saved_file_path = open('saved_file_path.txt', 'w')
-    saved_file_path.write(file_path)
-    saved_file_path.close()
-
-
-def get_event_info(component):
-    event_start = str(component.get('dtstart'))[10:29]
-    event_end = str(component.get('dtend'))[10:29]
-    event_name = str(component.get('summary'))
-    event_info = [event_start, event_end, event_name]
-    return event_info
-
-
-def find_events_this_week(start_date, events, event_info, weekday_nr):
-    today = date.today()
-    # if today.weekday() > weekday_nr and start_date == str(today + relativedelta(weekday=MO(-1))):
-    #     events[weekday_nr].append(event_info)
-    if start_date == str(today + relativedelta(weekday=weekday_nr)):
-        events[weekday_nr].append(event_info)
-
-
 def open_file():
     file_path = filedialog.askopenfilename(
         title='Open',
@@ -49,77 +137,33 @@ def open_file():
     save(file_path)
 
     for i in range(7):
-        text[i].delete('1.0', END)
+        textboxes[i].config(state=NORMAL)
+        textboxes[i].delete('1.0', END)
 
     display_calendar(file_path)
 
 
-def display_calendar(file_path):
-    file = open(file_path, 'rb')
-    calendar = Calendar.from_ical(file.read())
-    events = []
+def save(file_path):
+    saved_file_path = open('saved_file_path.txt', 'w')
+    saved_file_path.write(file_path)
+    saved_file_path.close()
+
+
+def update_days_numbers():
+    day_numbers = [None] * 7
+    weekday_abbr = [MO, TU, WE, TH, FR, SA, SU]
+    today = date.today()
 
     for i in range(7):
-        events.append([])
+        if today.weekday() > i:
+            day_numbers[i] = str(today + relativedelta(weekday=weekday_abbr[i](-1)))[8:11]
+        else:
+            day_numbers[i] = str(today + relativedelta(weekday=i))[8:11]
 
-    for component in calendar.walk():
-        if component.name == 'VEVENT':
-            event_info = get_event_info(component)
-            start_date = str(component.get('dtstart'))[10:20]
-
-            for i in range(7):
-                find_events_this_week(start_date, events, event_info, i)
-
-    for i in range(7):
-        events[i].sort()
-        text[i].config(state=NORMAL)
-
-    for event in range(len(events[2])):
-        text[2].insert(END, events[2][event][0][11:] + '-')
-        text[2].insert(END, events[2][event][1][11:] + '\n')
-        text[2].insert(END, events[2][event][2] + '\n')
-        #disabled text[]
-
-    file.close()
+    return day_numbers
 
 
 if __name__ == '__main__':
-    window = Tk()
-    window.title('Weekly Calendar')
-    window.resizable(False, False)
-
-    weekdays = ()
-    text = ()
-    padding_right = 0
-
-    for j in range(7):
-        weekdays = weekdays + (Label(text=day_name[j], font=('', 12)),)
-        text = text + (Text(height=10, width=30),)
-
-        if j < 4:
-            if j == 3:
-                padding_right = 10
-
-            weekdays[j].grid(row=0, column=j * 2, sticky=W, padx=10, pady=(10, 5))
-            text[j].grid(row=1, column=j * 2, columnspan=2, padx=(10, padding_right), pady=5)
-        else:
-            weekdays[j].grid(row=2, column=(j - 4) * 2, sticky=W, padx=10, pady=5)
-            text[j].grid(row=3, column=(j - 4) * 2, rowspan=3, columnspan=2, padx=(10, 0), pady=(5, 10))
-
-        text[j].config(state=DISABLED)
-
-    label_title = Label(text='Weekly Calendar', font=('', 16))
-    label_title.grid(row=3, column=6, columnspan=2, padx=10, pady=10)
-
-    label_month = Label(text='', font=('', 12))
-    label_month.grid(row=4, column=6, columnspan=2, padx=10, pady=10)
-
-    button = Button(
-        window,
-        text='Open an ICS file',
-        command=open_file
-    )
-    button.grid(row=5, column=6, columnspan=2, padx=10, pady=10)
-
+    program, textboxes = initialize_window()
     load()
-    window.mainloop()
+    program.mainloop()
